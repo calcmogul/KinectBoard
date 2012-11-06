a1c6b7d4320cf0407a049a80705eda0bff692718
Tyler Veness <calcmogul@gmail.com>
2012-11-05 18:44:54 -0800

    Removed duplicate message processing loop and made main() return correct value

--- src/Main.cpp
+++ src/Main.cpp
@@ -65,11 +65,6 @@ INT WINAPI WinMain( HINSTANCE Instance , HINSTANCE , LPSTR , INT ) {
     WindowClass.lpszClassName = mainClassName;
     RegisterClassEx(&WindowClass);
 
-    // Calibration window
-    HWND testWindow = CreateWindowEx( 0 , mainClassName , "KinectBoard" , WS_POPUP | WS_MINIMIZE , 0 , 0 , GetSystemMetrics(SM_CXSCREEN) , GetSystemMetrics(SM_CYSCREEN) , NULL , NULL , Instance , NULL );
-
-    testWin.create( testWindow );
-
     MSG Message;
     INPUT input = { 0 };
 
@@ -93,6 +88,22 @@ INT WINAPI WinMain( HINSTANCE Instance , HINSTANCE , LPSTR , INT ) {
             Instance ,
             NULL );
     /* =================================================== */
+
+    // Calibration window
+    HWND testWindow = CreateWindowEx( 0 ,
+            mainClassName ,
+            "KinectBoard" ,
+            WS_CHILD | WS_POPUP | WS_MINIMIZE ,
+            0 ,
+            0 ,
+            GetSystemMetrics(SM_CXSCREEN) ,
+            GetSystemMetrics(SM_CYSCREEN) ,
+            mainWindow ,
+            NULL ,
+            Instance ,
+            NULL );
+
+    testWin.create( testWindow );
 
     HGDIOBJ hfDefault = GetStockObject( DEFAULT_GUI_FONT );
 
@@ -118,46 +129,36 @@ INT WINAPI WinMain( HINSTANCE Instance , HINSTANCE , LPSTR , INT ) {
 
     bool lastConnection = true; // prevents window icon from being set every loop
 
-    while ( GetMessage(&Message, NULL, 0, 0) > 0 ) {
-        TranslateMessage( &Message );
-        DispatchMessage(&Message);
-    }
-
-    return (int) Message.wParam;
-
     while ( GetMessage( &Message , NULL , 0 , 0 ) > 0 ) {
-        if ( PeekMessage( &Message , NULL , 0 , 0 , PM_REMOVE ) ) {
-            // If a message was waiting in the message queue, process it
-            TranslateMessage( &Message );
-            DispatchMessage( &Message );
+        // If a message was waiting in the message queue, process it
+        TranslateMessage( &Message );
+        DispatchMessage( &Message );
+
+        // change color of icon to green or red if Kinect is connected or disconnected respectively
+        if ( projectorKinect.isConnected() && lastConnection == false ) { // if is connected and wasn't before
+            SendMessage( mainWindow , WM_SETICON , ICON_SMALL , reinterpret_cast<LPARAM>(kinectON) );
+            SendMessage( mainWindow , WM_SETICON , ICON_BIG , reinterpret_cast<LPARAM>(kinectON) );
+
+            lastConnection = true;
         }
-        else {
-            // change color of icon to green or red if Kinect is connected or disconnected respectively
-            if ( projectorKinect.isConnected() && lastConnection == false ) { // if is connected and wasn't before
-                SendMessage( mainWindow , WM_SETICON , ICON_SMALL , reinterpret_cast<LPARAM>(kinectON) );
-                SendMessage( mainWindow , WM_SETICON , ICON_BIG , reinterpret_cast<LPARAM>(kinectON) );
+        else if ( !projectorKinect.isConnected() && lastConnection == true ) { // if isn't connected and was before
+            SendMessage( mainWindow , WM_SETICON , ICON_SMALL , reinterpret_cast<LPARAM>(kinectOFF) );
+            SendMessage( mainWindow , WM_SETICON , ICON_BIG , reinterpret_cast<LPARAM>(kinectOFF) );
 
-                lastConnection = true;
-            }
-            else if ( !projectorKinect.isConnected() && lastConnection == true ) { // if isn't connected and was before
-                SendMessage( mainWindow , WM_SETICON , ICON_SMALL , reinterpret_cast<LPARAM>(kinectOFF) );
-                SendMessage( mainWindow , WM_SETICON , ICON_BIG , reinterpret_cast<LPARAM>(kinectOFF) );
-
-                lastConnection = false;
-            }
-
-            projectorKinect.processImage( Kinect::Red );
-            projectorKinect.display( mainWindow , 0 , 0 );
-
-            // TODO get images from Kinect, process them, and move mouse and press mouse buttons
-
-            //moveMouse( input , -5 , 0 ); // move mouse cursor 5 pixels left
-            //moveMouse( input , 5 , 0 ); // move mouse cursor 5 pixels right
-
-            //leftClick( input ); // left click for testing
-
-            Sleep( 50 );
+            lastConnection = false;
         }
+
+        projectorKinect.processImage( Kinect::Red );
+        projectorKinect.display( mainWindow , 0 , 0 );
+
+        // TODO get images from Kinect, process them, and move mouse and press mouse buttons
+
+        //moveMouse( input , -5 , 0 ); // move mouse cursor 5 pixels left
+        //moveMouse( input , 5 , 0 ); // move mouse cursor 5 pixels right
+
+        //leftClick( input ); // left click for testing
+
+        Sleep( 50 );
     }
 
     // Clean up windows
@@ -166,7 +167,7 @@ INT WINAPI WinMain( HINSTANCE Instance , HINSTANCE , LPSTR , INT ) {
     //DestroyWindow( mainWindow );
     UnregisterClass( mainClassName , Instance );
 
-    return EXIT_SUCCESS;
+    return Message.wParam;
 }
 
 LRESULT CALLBACK OnEvent( HWND Handle , UINT Message , WPARAM WParam , LPARAM LParam ) {
@@ -193,7 +194,7 @@ LRESULT CALLBACK OnEvent( HWND Handle , UINT Message , WPARAM WParam , LPARAM LP
                     // TODO process Kinect image to find location of Kinect in 3D space
                 }
 
-                ShowWindow( testWin.getSystemHandle() , SW_MINIMIZE | SW_HIDE );
+                ShowWindow( testWin.getSystemHandle() , SW_MINIMIZE );
             }
             break;
         }
@@ -273,7 +274,7 @@ void drawTestPattern( sf::RenderWindow& targetWin , const sf::Color& outlineColo
     border.setPosition( 20.f , 20.f );
 
     // Make window visible before drawing to it
-    ShowWindow( targetWin.getSystemHandle() , SW_MAXIMIZE );
+    ShowWindow( targetWin.getSystemHandle() , SW_SHOWMAXIMIZED );
 
     // Draw graphics to window
     targetWin.clear( sf::Color( 0 , 0 , 0 ) );
